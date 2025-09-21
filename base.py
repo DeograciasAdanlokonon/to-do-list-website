9fb0a774a5fc86df43efdcee7e9dd08167e1c535
DeograciasAdanlokonon <adanlokonondeogracias1@gmail.com>
2025-09-21 09:23:37 +0100

    third commit

--- base.py
+++ base.py
@@ -43,7 +43,10 @@ class DataBase():
   """Initiate db"""
   def __init__(self, app):
     self.app = app
-    self.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI', 'sqlite:///todos.db')
+    self.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+            'DATABASE_URL', 'sqlite:///instance/todos.db'
+        )
+    self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(self.app)
 
     with self.app.app_context():
--- main.py
+++ main.py
@@ -12,6 +12,8 @@ Bootstrap5(app=app)
 # Flask Login Manager
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "warning"
 
 # user loader callback
 @login_manager.user_loader
